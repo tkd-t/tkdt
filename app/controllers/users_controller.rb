1f148abf1e9795fd5ecb9f31126c7676c9009f5d
TT <takeda.tky@outlook.jp>
2020-02-21 06:22:22 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,41 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.new(user_params)   # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
       log_in @user
-      redirect_to @user
       flash[:success] = "Welcome to the Sample App!"
-      # 保存の成功をここで扱う。
+      redirect_to @user
     else
       render 'new'
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-  
 end
